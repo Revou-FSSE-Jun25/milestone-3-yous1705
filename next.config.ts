e7191f7a56c55d8e875d9cae1f4b92103e6a95ce
Yous Sibarani <youssibarani17@gmail.com>
2025-10-11 01:16:39 +0700

    try liveserver

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   images: { unoptimized: true },
-  basePath: "/milestone-3-yous1705",
-  assetPrefix: "/milestone-3-yous1705/",
 };
 
 export default nextConfig;
