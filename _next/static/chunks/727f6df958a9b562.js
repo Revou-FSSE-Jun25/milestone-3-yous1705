25a08700961db839d89fe8cd4daa076017c4c9d3
Yous Sibarani <youssibarani17@gmail.com>
2025-10-11 00:25:44 +0700

    Updates

---
+++ _next/static/chunks/727f6df958a9b562.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/7e16147ef720fd43.js",
+  "static/chunks/turbopack-42aabbc71b1a6d6f.js"
+])
---
+++ _next/static/tvIy1tEWJRoXXquv0Xg6H/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/1b5bcdce5245aeac.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/milestone-3-yous1705//_next/:path+",
+        "destination": "/milestone-3-yous1705/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
