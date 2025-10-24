f519dafd287f197c8a86e6258bb2bfecce0961c8
Yous Sibarani <youssibarani17@gmail.com>
2025-10-25 05:19:18 +0700

    fixstatic param

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/milestone-3-yous1705",
+  assetPrefix: "/milestone-3-yous1705/",
   images: {
     domains: ["cdn.dummyjson.com", "image.google.com"],
     remotePatterns: [],
